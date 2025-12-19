b73f4514e7776b0c1cac3fded865b8d7579cd6af
Firato <142419225+Firat-o@users.noreply.github.com>
2025-12-19 23:36:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { NotFound } from "./pages/NotFound"
-import {Toaster} from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster"
 
 function App() {
-
   return (
     <>
       <Toaster />
       <BrowserRouter>
-      <Routes>
-        <Route index element ={<Home />}/>
-        <Route path='*' element={<NotFound />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </>
   )
