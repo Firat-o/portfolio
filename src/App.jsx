02c25af68c0ab4de86991476425ae13b549c0273
Firato <142419225+Firat-o@users.noreply.github.com>
2026-03-26 09:51:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+
+// 1. Die neuen Seiten importieren
+import Impressum from "./pages/Impressum";
+import Privacy from "./pages/Privacy";
+
 import { Toaster } from "@/components/ui/toaster";
 import { CustomCursor } from "./components/CustomCursor";
 
@@ -12,6 +17,11 @@ const AnimatedRoutes = () => {
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
+        
+        {/* 2. Die neuen Routen registrieren */}
+        <Route path="/impressum" element={<Impressum />} />
+        <Route path="/privacy" element={<Privacy />} />
+        
         <Route path="*" element={<NotFound />} />
       </Routes>
     </AnimatePresence>
